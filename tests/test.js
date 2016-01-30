bf9b53d32e9fc8cae871288b1e0cb008e7dcd12e
Red-French <red@redfrench.net>
2016-01-30 14:25:36 -0600

    calendar header printing

--- tests/test.js
+++ tests/test.js
@@ -9,7 +9,7 @@ const { execSync } = require('child_process');  // es6 syntax
 
 describe('cal', () => {
   describe('CLI', () => {
-    xit('should handle the current month', () => {
+    it('should handle the current month', () => {
       const goal = execSync('cal').toString();
       const output = execSync('./lib/cal.js').toString();
 
@@ -20,40 +20,40 @@ describe('cal', () => {
   describe("Zeller's congruence", () => {
     // const zellers = require('../lib/zellars.js');
     
-    describe('.modifiedMonth', () => {
+    describe('.getMonth', () => {
       it('return 13 for January', () => {
-        const mod = zellers.modifiedMonth(2012, 1);
-        const mod2 = zellers.modifiedMonth(2015, 2);
-        //const mod3 = zellers.modifiedMonth(2016, 3);  // one way to write test (part1); part2 is below
+        const mod = zellers.getMonth(2012, 1);
+        const mod2 = zellers.getMonth(2015, 2);
+        //const mod3 = zellers.getMonth(2016, 3);  // one way to write test (part1); part2 is below
         // console.log(zellers);
-        expect(zellers.modifiedMonth(2012, 1)).to.equal(13);
+        expect(zellers.getMonth(2012, 1)).to.equal(13);
         expect(mod2).to.equal(14)
         //expect(mod3).to.equal(3);  // part2; goes with part1 above
-        expect(zellers.modifiedMonth(2016,3)).to.equal(3); // more concise way to write the above test
+        expect(zellers.getMonth(2016,3)).to.equal(3); // more concise way to write the above test
       });
       // 2 === 14
       // 3 === 3
     });
 
-    describe('.modifiedYear', () => {
+    describe('.getYear', () => {
       it('returns 2015 for Jan 2015', () => {
-//        const mod = zellers.modifiedYear(2015, 1);
+//        const mod = zellers.getYear(2015, 1);
 //        expect(mod).to.equal(2014);
-        expect(zellers.modifiedYear(2015, 1)).to.equal(2014);
-        expect(zellers.modifiedYear(2016, 2)).to.equal(2015);
-        expect(zellers.modifiedYear(2017, 3)).to.equal(2017);
+        expect(zellers.getYear(2015, 1)).to.equal(2014);
+        expect(zellers.getYear(2016, 2)).to.equal(2015);
+        expect(zellers.getYear(2017, 3)).to.equal(2017);
       });
     });
 
     describe('.getDay', () => {
       it('returns 3 (Tuesday) for March 1, 2016', () => {
-        const mod = zellers.getDay(2014, 3, 2);
+        const mod = zellers.getDay(2014, 3, 1);
 
-        expect(mod).to.equal(1);
+        expect(mod).to.equal(0);
         //expect(zellers.getDay(2014, 3, 2)).to.equal(1);  // more concise way to write the above test
         expect(zellers.getDay(2000, 3, 1)).to.equal(4);
         expect(zellers.getDay(2100, 3, 1)).to.equal(2);
-        expect(zellers.getDay(2200, 3, 2)).to.equal(1);
+        expect(zellers.getDay(2200, 3, 1)).to.equal(0);
         expect(zellers.getDay(2300, 3, 1)).to.equal(5);
         expect(zellers.getDay(2016, 1, 1)).to.equal(6);
       });
@@ -63,22 +63,29 @@ describe('cal', () => {
   describe("TEST MONTH.JS", () => {
     describe('.grabDate', () => {
       it('grabs the date, dude', () => {
-        expect(getDate.grabDate().today).to.equal(29);  // checks current date from month.js
-        expect(getDate.grabDate().currentMonth).to.equal(1);  // checks current month from month.js
-        expect(getDate.grabDate().currentYear).to.equal(2016); // checks current year from month.js
+        // expect(getDate.grabDate().today).to.equal(29);  // checks current date from month.js
+        // expect(getDate.grabDate().currentMonth).to.equal(1);  // checks current month from month.js
+        // expect(getDate.grabDate().currentYear).to.equal(2016); // checks current year from month.js
+        expect(getDate.displayHeader(1, 2016)).to.equal('    January 2016\nSu Mo Tu We Th Fr Sa\n');
+        expect(getDate.displayHeader(5, 2016)).to.equal('      May 2016\nSu Mo Tu We Th Fr Sa\n');
+        expect(getDate.displayHeader(8, 2016)).to.equal('    August 2016\nSu Mo Tu We Th Fr Sa\n');
+        expect(getDate.displayHeader(6, 2016)).to.equal('     June 2016\nSu Mo Tu We Th Fr Sa\n');
+        expect(getDate.displayHeader(9, 2016)).to.equal('   September 2016\nSu Mo Tu We Th Fr Sa\n');
+        expect(getDate.displayHeader(9, 700)).to.equal('   September 700\nSu Mo Tu We Th Fr Sa\n');
       });
-    });    
+    });
   });
-  
-  describe("TEST CAL.JS", () => {
-    describe('.buildCal', () => {
-      it('build the calendar, my man', () => {
-        expect(cal.buildCal().thisMonth).to.equal(1); // checks current month from cal.js
-        expect(cal.buildCal().thisDay).to.equal(29); // checks current day from cal.js
-        expect(cal.buildCal().thisYear).to.equal(2016)  // checks current year from cal.js
-      });
-    });    
-  });
+
+
+  // describe("TEST CAL.JS", () => {
+  //   describe('.buildCal', () => {
+  //     it('build the calendar, my man', () => {
+  //       expect(cal.buildCal().thisMonth).to.equal(1); // checks current month from cal.js
+  //       expect(cal.buildCal().thisDay).to.equal(29); // checks current day from cal.js
+  //       expect(cal.buildCal().thisYear).to.equal(2016)  // checks current year from cal.js
+  //     });
+  //   });    
+  // });
 
 
 });
